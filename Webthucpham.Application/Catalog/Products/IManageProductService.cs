666c229a16a9c3022c4d95d5e2d7f191d4edef37
Tùng Bùi <no.problem.161120@gmail.com>
2021-08-11 20:43:19 +0700

    Add project files.

---
+++ Webthucpham.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Webthucpham.Application.Catalog.Products.Dtos;
+using Webthucpham.Application.Catalog.Products.Dtos.Manage;
+using Webthucpham.Application.Dtos;
+
+namespace Webthucpham.Application.Catalog.Products
+{
+    public interface IManageProductService //quan ly
+    {
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductUpdateRequest request);
+        Task<int> Delete(int productId);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task AddViewcount(int productId);
+
+   
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+
+    }
+}
---
+++ Webthucpham.Data/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Webthucpham.Data.Entities;
+
+namespace Webthucpham.Data.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions"); // tên bảng
+           
+            builder.HasKey(x => x.Id);
+             
+            builder.Property(x => x.Id).UseIdentityColumn();
+            // one with many
+            builder.HasOne(x => x.AppUser).WithMany(x => x.Transactions).HasForeignKey(x => x.UserID);
+        }
+    }
+}
